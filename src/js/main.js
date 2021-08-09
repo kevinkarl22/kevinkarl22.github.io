12c35a8cb216c8b1e0f7b2c47298cf7a2a3b38c9
kevinkarl22 <ReinerGwanpil07@yahoo.com>
2021-08-09 08:48:18 +0800

    Updated

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,17 @@
 $(function() {
-  var pages = ['1', 'previewer-box', 'version', 'developer', 'report-a-bug'];
+  var pages = ['home', 'features', 'products'];
   var currentPage = window.location.hash.replace('#', '');
 
-  var linkPreviewerNavLink = $('#link-previewer .nav-link');
-  var linkPreviewerMainContent = $('#link-previewer #main-content');
-  var linkPreviewerSidebarMenu = $('#link-previewer #sidebar-menu');
-  var linkPreviewerNavItem = $('#link-previewer .nav-item a[href="#previewer-box"]');
+  var navLink = $('#main-body .nav-link');
+  var navItem = $('#main-body .nav-link[href="#home"]');
+  var mainContent = $('#main-body #main-content');
 
   function setActiveMenu() {
-    var linkPreviewerNavLinkActive = $('#link-previewer .nav-link.active');
+    var navLinkActive = $('#main-body .nav-link.active');
 
-    linkPreviewerNavLinkActive.removeClass('active');
+    navLinkActive.removeClass('active');
 
-    linkPreviewerNavLink.each(function() {
+    navLink.each(function() {
       if ($(this).attr('href').replace('#', '') == currentPage) {
         $(this).attr('aria-current', 'page').addClass('active');
       }
@@ -25,50 +24,10 @@ $(function() {
         type: 'GET',
       })
       .done(function(data) {
-        linkPreviewerMainContent.html(data);
+        mainContent.html(data);
 
         switch (currentPage) {
-          case 'previewer-box':
-            var linkPreviewerEnablePreviewerBox = $('#link-previewer #enable-previewer-box');
-            var linkPreviewerBoxSize = $('#link-previewer #box-size');
-            var linkPreviewerBorderStyle = $('#link-previewer #border-style');
-            var linkPreviewerShowLinkInfo = $('#link-previewer #show-link-info');
-            var linkPreviewerSaveChangesButton = $('#link-previewer #save-changes-button');
-            var linkPreviewerFormMessage = $('#link-previewer #form-message');
-
-            chrome.storage.sync.get(({
-              lpEnablePreviewerBox,
-              lpBoxSize,
-              lpBorderStyle,
-              lpShowLinkInfo
-            }) => {
-              linkPreviewerEnablePreviewerBox.attr('checked', lpEnablePreviewerBox);
-              linkPreviewerBoxSize.val(lpBoxSize);
-              linkPreviewerBorderStyle.val(lpBorderStyle);
-              linkPreviewerShowLinkInfo.attr('checked', lpShowLinkInfo);
-            });
-
-            linkPreviewerSaveChangesButton.on('click', function() {
-              let lpEnablePreviewerBox = (linkPreviewerEnablePreviewerBox.is(':checked')) ? true : false;
-              let lpBoxSize = linkPreviewerBoxSize.val();
-              let lpBorderStyle = linkPreviewerBorderStyle.val();
-              let lpShowLinkInfo = (linkPreviewerShowLinkInfo.is(':checked')) ? true : false;
-
-              chrome.storage.sync.set({
-                lpEnablePreviewerBox,
-                lpBoxSize,
-                lpBorderStyle,
-                lpShowLinkInfo
-              });
-
-              linkPreviewerFormMessage.html('<div class="alert alert-success alert-dismissible fade show" role="alert">Changes Successfully Saved.<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>');
-
-              var linkPreviewerAlert = $('#link-previewer .alert');
-
-              setTimeout(function() {
-                linkPreviewerAlert.alert('close');
-              }, 2000)
-            });
+          case 'home':
             break;
           default:
         }
@@ -79,19 +38,15 @@ $(function() {
     currentPage = window.location.hash.replace('#', '');
 
     if ($.inArray(currentPage, pages) == -1) {
-      linkPreviewerNavItem.get(0).click();
+      navItem.get(0).click();
     } else {
       setActiveMenu();
       loadPage();
     }
   });
 
-  linkPreviewerNavLink.on('click', function() {
-    linkPreviewerSidebarMenu.collapse('hide');
-  });
-
   if (currentPage == '') {
-    linkPreviewerNavItem.get(0).click()
+    navItem.get(0).click()
   } else {
     setActiveMenu();
     loadPage();
